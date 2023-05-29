b4d02d3d75353018036b8da30dc4e221cb1ce0f8
Rose Gates <pedroluiz2109@gmail.com>
2023-05-29 17:11:29 -0300

    add: seed database

--- SalesWeb/Program.cs
+++ SalesWeb/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddDbContext<SalesWebContext>(options =>
     new MySqlServerVersion(new Version(8, 0, 26)),
     builder => builder.MigrationsAssembly("SalesWeb")));
 
+builder.Services.AddScoped<SeedingService>();
+
 // Resto do código...
 
 // Add services to the container.
@@ -17,11 +19,16 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+} 
+else
+{
+    app.UseDeveloperExceptionPage();
+    SeedingService.Seed();
 }
 
 app.UseHttpsRedirection();
