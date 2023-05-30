1858c9742695c84c8e26b93dcd0b4eb181290a57
Rose Gates <pedroluiz2109@gmail.com>
2023-05-30 00:57:10 -0300

    impl: view select department in create seller

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -19,7 +19,6 @@ namespace SalesWeb.Services
 
         public void Insert(Seller seller)
         {
-            seller.Department = _context.Department.First();
             _context.Add(seller);
             _context.SaveChanges();
         }
